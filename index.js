d6dda66c2a8977e766bec311c5ee928790e4df6a
Andrew <armartin1998@gmail.com>
2023-03-03 09:59:51 -0500

    Complete filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driver, name){
+    const name1 = name.toUpperCase()
+    const matchingDrivers = driver.filter(string => string.toUpperCase() === name1)
+    return matchingDrivers
+}
+
+function fuzzyMatch(driver, name){
+    const beginningLetter = name.slice(0,1)
+    const matchingDrivers = driver.filter(string => string.slice(0,1) === beginningLetter)
+    return matchingDrivers
+}
+
+function matchName(driver, name){
+    const matchingDrivers = driver.filter(string => string.name === name)
+    return matchingDrivers
+}
